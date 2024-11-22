9e3c93f6f929d3e916a7031cf7368f1908cad27f
Bassim Shahidy <bassim101@gmail.com>
2024-11-22 14:46:52 -0500

    nvim: Use autocmd to apply custom WinSeparator color

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -8,8 +8,14 @@
 --   nested = true,
 -- })
 
-vim.cmd([[highlight WinSeparator guifg=#3C3F47]])
+-- Set window separator highlight for all cases
+vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter" }, {
+  callback = function()
+    vim.cmd([[highlight WinSeparator guifg=#3C3F47]])
+  end,
+})
 
+-- Disable autoformatting for nix files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "nix" },
   callback = function()
