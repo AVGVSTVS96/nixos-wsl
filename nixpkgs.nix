1418354bb95fabd196a546b0fe9b0f706f3a6c23
Bassim Shahidy <bassim101@gmail.com>
2024-11-22 14:46:18 -0500

    Add overlay to update graphite-cli

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,8 @@
 { inputs, ... }:
 let
   inherit (inputs) nixpkgs-stable nur;
-in {
+in
+{
   nixpkgs = {
     config = {
       allowUnfree = true;
@@ -14,6 +15,15 @@ in {
         stable = import nixpkgs-stable {
           inherit (prev) system config;
         };
+      })
+      (final: prev: {
+        graphite-cli = prev.graphite-cli.overrideAttrs (oldAttrs: {
+          version = "1.4.8";
+          src = prev.fetchurl {
+            url = "https://registry.npmjs.org/@withgraphite/graphite-cli/-/graphite-cli-1.4.8.tgz";
+            hash = "sha256-PzoMDBoWAXVbK3itcpybpjZ+lWd8tS7UOFtWWMwTh5U=";
+          };
+        });
       })
     ];
   };
