059fa266f3d11922b1808befe27a71aed02d4ff3
Bassim Shahidy <bassim101@gmail.com>
2025-04-07 13:00:58 -0400

    update, add graphite overlay, fix tz typo

--- nix.nix
+++ nix.nix
@@ -22,15 +22,15 @@ in
           inherit (prev) system config;
         };
       })
-      # (final: prev: {
-      #   graphite-cli = prev.graphite-cli.overrideAttrs (oldAttrs: {
-      #     version = "1.4.8";
-      #     src = prev.fetchurl {
-      #       url = "https://registry.npmjs.org/@withgraphite/graphite-cli/-/graphite-cli-1.4.8.tgz";
-      #       hash = "sha256-PzoMDBoWAXVbK3itcpybpjZ+lWd8tS7UOFtWWMwTh5U=";
-      #     };
-      #   });
-      # })
+      (final: prev: {
+        graphite-cli = prev.graphite-cli.overrideAttrs (oldAttrs: {
+          version = "1.5.3";
+          src = prev.fetchurl {
+            url = "https://registry.npmjs.org/@withgraphite/graphite-cli/-/graphite-cli-1.5.3.tgz";
+            hash = "sha256-hWr4HOpcNXEpdboeHige5nliVCLY3RukMVh2xRKGIlI=";
+          };
+        });
+      })
       (final: prev: {
         bun = prev.bun.overrideAttrs (oldAttrs: {
           version = "1.2.2";
